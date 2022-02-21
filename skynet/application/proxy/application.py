a34d3a3f2ec24e8488a0d3a61a4b8f2b968d96c5
Andrea F. Daniele <afdaniele@ttic.edu>
2022-02-20 22:36:20 -0600

    added events callbacks, shutdown sequence, storage_path()

--- skynet/application/proxy/application.py
+++ skynet/application/proxy/application.py
@@ -1,11 +1,28 @@
-from typing import Dict
+import os
+import signal
+from collections import defaultdict
+from threading import Semaphore
+from typing import Dict, Callable, Set
 
+from skynet.application.logging import salogger
 from skynet.application.proxy.node import Node
 from skynet.application.types import SkynetService
+
+EventName = str
+CallbackPriority = int
 
 
 class SkynetApplication:
     services: Dict[str, SkynetService] = {}
+
+    # events
+    events_callbacks: Dict[EventName, Dict[CallbackPriority, Set[Callable]]] = defaultdict(
+        lambda: defaultdict(set))
+    events_lock: Semaphore = Semaphore()
+
+    @classmethod
+    def storage_path(cls, fpath: str = "") -> str:
+        return os.path.join("/storage", fpath).rstrip("/")
 
     @classmethod
     def expose(cls, service: SkynetService):
@@ -15,3 +32,26 @@ class SkynetApplication:
         cls.services[service.name] = service
         # notify the node
         Node.call("service/expose", service.serialize())
+
+    @classmethod
+    def _on_event_shutdown(cls):
+        with cls.events_lock:
+            callbacks = cls.events_callbacks["shutdown"]
+            for priority in sorted(callbacks.keys(), reverse=True):
+                for cb in callbacks[priority]:
+                    cb()
+
+    @classmethod
+    def _on_sigint_signal(cls, _, __):
+        salogger.info("Received shutdown request. Terminating gracefully...")
+        cls._on_event_shutdown()
+        exit(0)
+
+    @classmethod
+    def on_shutdown(cls, callback: Callable, priority: CallbackPriority = 0):
+        with cls.events_lock:
+            cls.events_callbacks["shutdown"][priority].add(callback)
+
+
+# noinspection PyProtectedMember
+signal.signal(signal.SIGINT, SkynetApplication._on_sigint_signal)
